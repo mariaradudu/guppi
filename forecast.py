bdf4adb49c02702cb52d0163353452e95b26b963
mariaradudu <mariaradu@ukr.net>
2026-07-15 19:30:08 +0300

    Create forecast.py

---
+++ forecast.py
@@ -0,0 +1,16 @@
+class Forecast:
+
+    def __init__(
+        self,
+        city,
+        condition,
+        temperature,
+        humidity,
+        wind
+    ):
+
+        self.city = city
+        self.condition = condition
+        self.temperature = temperature
+        self.humidity = humidity
+        self.wind = wind
