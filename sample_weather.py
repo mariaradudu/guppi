ee2b5a2588dccb65dfc8cbe8f31a55a5e3649c3c
mariaradudu <mariaradu@ukr.net>
2026-07-15 19:30:41 +0300

    Create sample_weather.py

---
+++ sample_weather.py
@@ -0,0 +1,39 @@
+DATA = {
+
+    "Rome": {
+
+        "condition": "Sunny",
+
+        "temperature": 28,
+
+        "humidity": 43,
+
+        "wind": 12
+
+    },
+
+    "Berlin": {
+
+        "condition": "Cloudy",
+
+        "temperature": 22,
+
+        "humidity": 61,
+
+        "wind": 17
+
+    },
+
+    "Madrid": {
+
+        "condition": "Clear",
+
+        "temperature": 31,
+
+        "humidity": 37,
+
+        "wind": 9
+
+    }
+
+}
